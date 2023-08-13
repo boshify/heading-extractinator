5948f58295b4e2cd71a6a90ce5a991266b7ee323
boshify <140970186+boshify@users.noreply.github.com>
2023-08-13 11:55:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,34 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-def extract_headers(url):
-    headers_list = []
-    try:
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"}
-        response = requests.get(url, headers=headers)
-        soup = BeautifulSoup(response.content, 'html.parser')
-        for header in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
-            headers_list.append(f"{header.name}: {header.text.strip()}")
-    except Exception as e:
-        st.write(f"An error occurred: {e}")
-    return headers_list
+def extract_headings(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
+    }
+    
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    headings = []
+    for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
+        indent = '    ' * (int(tag.name[1]) - 1)
+        headings.append(f"{indent}<{tag.name}>{tag.text.strip()}</{tag.name}>")
+    
+    return headings
 
 st.title('Website Header Extractor')
 
-url = st.text_input('Enter a URL:', '')
-if url:
-    headers = extract_headers(url)
-    for header in headers:
-        st.write(header)
+urls = []
+for i in range(1, 7):
+    url = st.text_input(f"Enter URL {i}:", "")
+    if url:
+        urls.append(url)
+
+if st.button("Extract Headers"):
+    for url in urls:
+        st.write(f"URL: {url}")
+        headings = extract_headings(url)
+        for heading in headings:
+            st.markdown(heading, unsafe_allow_html=True)
